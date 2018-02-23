5d33f92d4821e20d1c0e06452a81c093b34ddf76
Ahsan Zaman <ahsanzam@usc.edu>
2018-02-23 09:52:06 -0800

    Added delayed-playing functionality to sounds

--- js/Sounds.js
+++ js/Sounds.js
@@ -16,35 +16,56 @@ Non-ambient sounds will automatically stop playing once done.
 class Sounds{
 	constructor(){
 		this.ambientNoiseVolume = 0.2;
-		this.sunnyAmbience = new Howl({
-		  src: ['assets/Sounds/sunny-ambient-noise.mp3'],
-		  loop: true,
-		  volume: 0
-		});
+		this.loadFinished = false;
+		this.queue = Array();
+		this.soundsDict = {	"sunnyAmbience" : new Howl({
+								  src: ['assets/Sounds/sunny-ambient-noise.mp3'],
+								  loop: true,
+								  volume: 0
+								})
+							};
 		var that = this;
-		setTimeout(() => { that.loadSounds(); }, 5000);
+		setTimeout(() => { that.loadSounds(); }, 5000); //asynchronously load other sounds
 
 	}
 	loadSounds(){
-		this.piratesArrgh = new Howl({src: ['assets/Sounds/pirate-arrgh.mp3']});
-		this.buoyBells = new Howl({src: ['assets/Sounds/buoy-bells.mp3']});
-		this.lightningSound = new Howl({src: ['assets/Sounds/lightning.mp3']});
-		this.stormyAmbience = new Howl({
-		  src: ['assets/Sounds/storm-ambient-noise.mp3'],
-		  loop: true,
-		  volume: 0,
-		  autoplay:false
-		});
-		this.dockedAmbience = new Howl({
-			src: ['assets/Sounds/docked-ambient-noise.mp3'],
-			loop: true,
-			volume: 0,
-			autoplay: false
-		});
+		Object.assign(this.soundsDict, this.soundsDict, {  	"piratesArrgh" : new Howl({src: ['assets/Sounds/pirate-arrgh.mp3']}),
+															"buoyBells" : new Howl({src: ['assets/Sounds/buoy-bells.mp3']}),
+															"lightningSound" : new Howl({src: ['assets/Sounds/lightning.mp3']}),
+															"stormyAmbience" : new Howl({
+															  src: ['assets/Sounds/storm-ambient-noise.mp3'],
+															  loop: true,
+															  volume: 0,
+															  autoplay:false
+															}),
+															"dockedAmbience" : new Howl({
+																src: ['assets/Sounds/docked-ambient-noise.mp3'],
+																loop: true,
+																volume: 0,
+																autoplay: false
+															})
+														})
+		this.loadFinished = true;
+		this.executeQueue();
 	}
-	fadeInOut(sound, fromVolume, toVolume, duration){
-		if(toVolume != 0) sound.play();
-		sound.fade(fromVolume, toVolume,duration);
-		if(toVolume == 0) sound.stop();
+	fadeInOut(soundKey, fromVolume, toVolume, duration){
+		if(this.soundsDict[soundKey] == null && !this.loadFinished){ //if this function is called before selected sound is loaded, wait for load to complete
+			if(soundKey != "stormyAmbience" && soundKey != "dockedAmbience" && soundKey != "sunnyAmbience") 
+				return; //we only care about the looped sounds being played late; others would sound out of time
+			var that = this;
+			this.queue.push( function(){ that.fadeInOut(soundKey, fromVolume, toVolume, duration) } );
+			return;
+		}
+		if(this.soundsDict[soundKey] == null) {
+			console.log("Key "+soundKey+" does not exist."); //if load is finished is key still does not exist, error
+			return; 
+		}
+		if(toVolume != 0) this.soundsDict[soundKey].play();
+		this.soundsDict[soundKey].fade(fromVolume, toVolume,duration);
+		if(toVolume == 0) this.soundsDict[soundKey].stop();
+	}
+	executeQueue(){
+		if(!this.loadFinished) return; 
+		while(this.queue.length > 0) this.queue.pop()();
 	}
 }
